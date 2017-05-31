22eee617e2f9b7a4c9f682d5c709458a08bb1d3f
Billy Chang <billyheungwing@yahoo.ca>
2017-05-31 19:09:06 +0000

    added new recipe json files

--- getRecipeFromNode.js
+++ getRecipeFromNode.js
@@ -12,9 +12,9 @@ const fs = require('fs');
 //             fs.writeFile('jsonfile.json', json)
 //         });
 
-unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=45")
+unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/random?limitLicense=false&number=50")
     .header("X-Mashape-Key", "2HS3QW2ddJmshmp1P69T5wIg21Wap1bULZxjsnMxKtafayYhn1")
     .header("Accept", "application/json")
     .end(function (result) {
-      fs.writeFile('jsonfile.json', JSON.stringify(result))
+      fs.writeFile('jsonfile05312017.json', JSON.stringify(result))
     });
